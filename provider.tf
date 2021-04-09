b034b571d493253f1a612d36b6ada305b6c15013
beanxyz <yuan.li@live.com>
2021-04-10 00:44:59 +1000

    'test'

--- provider.tf
+++ provider.tf
@@ -1,15 +1,17 @@
 provider "aws" {
   version = "~> 3.0"
-  profile = var.aws_profile
+  # profile = var.aws_profile
+  access_key = secrets.AWS_ACCESS_KEY_ID_STAGING
+  secret_key = secrets.AWS_SECRET_ACCESS_KEY_STAGING
   region  = "ap-southeast-2"
 }
 
-provider "aws" {
-  version = "~> 3.0"
-  alias   = "edge_provider"
-  profile = var.aws_profile
-  region  = "a-southeast-2"
-}
+# provider "aws" {
+#   version = "~> 3.0"
+#   alias   = "edge_provider"
+#   profile = var.aws_profile
+#   region  = "a-southeast-2"
+# }
 
 # The backend config variables come from a backend.tfvars file
 # which is not in this repo

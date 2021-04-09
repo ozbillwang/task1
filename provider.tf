3b34956b3b8dd6767f151d8aa00894339d9bcf61
beanxyz <yuan.li@live.com>
2021-04-09 23:56:41 +1000

    fix variable

--- provider.tf
+++ provider.tf
@@ -1,3 +1,38 @@
 provider "aws" {
-  region = "ap-southeast-2"
+  version = "~> 3.0"
+  profile = var.aws_profile
+  region  = "ap-southeast-2"
+}
+
+provider "aws" {
+  version = "~> 3.0"
+  alias   = "edge_provider"
+  profile = var.aws_profile
+  region  = "a-southeast-2"
+}
+
+# The backend config variables come from a backend.tfvars file
+# which is not in this repo
+terraform {
+  backend "s3" {
+  }
+}
+
+resource "aws_s3_bucket" "test_results" {
+  bucket = var.test_results_bucket_name
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    product     = "common"
+    platform    = "common"
+    bucket_name = var.test_results_bucket_name
+  }
 }
--- tfvars/staging/st-test-results-bucket_ap-southeast-1.tfvars
+++
@@ -1,2 +0,0 @@
-instance-type = "t2.micro"
-external_ip   = "0.0.0.0/0"
